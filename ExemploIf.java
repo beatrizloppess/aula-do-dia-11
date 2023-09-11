68ab94dbc19b0d3b78dfa3ab6d1c7b2b56d8b0c2
beatriz Santos <beatrizsantoslopes8@gmail.com>
2023-09-11 20:46:44 -0300

    ap03(selecao): Atualizacao

---
+++ ExemploIf.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+public class ExemploIf{
+  static public void main (String []args){                        
+   //declaração de variavel 
+
+   //entrada
+
+   //processamento
+d
+
+   //saída
+}
+}
